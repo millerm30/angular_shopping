99ab3836660e1a766f845e51a8508db9b7135871
millerm30 <millergm30@gmail.com>
2023-01-29 12:41:11 -0330

    cleanup

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -10,15 +10,19 @@ export class CartService {
   constructor(
     private http: HttpClient
   ) {}
+
   getShippingPrices() {
     return this.http.get<{type: string, price: number}[]>('/assets/shipping.json');
   }
+
   addToCart(product: Product) {
     this.items.push(product);
   }
+
   getItems() {
     return this.items;
   }
+  
   clearCart() {
     this.items = [];
     return this.items;
